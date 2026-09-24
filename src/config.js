e6f2c520bdc1c7b5ee0f727f377880caf07944cb
Reyhan Labs <syaiful.amrie87@gmail.com>
2026-09-24 11:06:16 +0700

    deploy_v4.19.2

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /** App version & shared constants */
-export const APP_VERSION = '4.19.1';
+export const APP_VERSION = '4.19.2';
 export const APP_VERSION_DATE = '2026-09-24';
-export const APP_VERSION_NOTE = 'Priority colors: Immediate red, Low green';
+export const APP_VERSION_NOTE = 'Fix duplicate tester table+cards layout';
 
 export const RFT_STATUS_CACHE_KEY = 'erp_rft_status_id';
 export const MIGRATE_SNAP_KEY = 'erp_migrate_snapshot_v1';
